44bdb6c6b9bb165b39739aa099282826ce935ac8
Shinichi Tomita <shinichi.tomita@gmail.com>
2020-09-14 13:03:53 +0900

    change worker to execute heavy calculation

--- src/example/worker.ts
+++ src/example/worker.ts
@@ -1,9 +1,19 @@
-async function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(() => resolve(), ms));
+function veryVeryHeavyLogic(num: number) {
+  let sum = 0;
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      for (let k = 0; k < num; k++) {
+        sum += i + j + k;
+      }
+    }
+  }
+  return sum;
 }
 
 self.addEventListener("message", async (e) => {
   console.log("message arrived to web worker", e);
-  await delay(1000);
-  self.postMessage(e.data);
+  veryVeryHeavyLogic(2000);
+  const reversed = e.data.split("").reverse().join("");
+  console.log("message is sent back to client", reversed);
+  self.postMessage(reversed);
 });
